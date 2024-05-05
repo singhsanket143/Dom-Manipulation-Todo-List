c437a7cf6c8991ee0bbf6033d05ea210a33ac5b2
Sanket Singh <singhsanket143@gmail.com>
2024-05-05 17:32:57 +0530

    started showing todolist

--- script.js
+++ script.js
@@ -11,11 +11,23 @@ function addTodoToLocalStorage(todoText) {
     localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+function appendTodoInHtml(todoText) {
+    const todoList = document.getElementById("todoList");
+
+    const todo = document.createElement("li");
+
+    todo.textContent = todoText;
+
+    todoList.appendChild(todo)
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const todoInput = document.getElementById("todoInput");
 
     const submitButton = document.getElementById("addTodo");
+
+    const todoList = document.getElementById("todoList");
 
     submitButton.addEventListener("click", (event) => {
         const todoText = todoInput.value;
@@ -23,6 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Please write something for the todo");
         } else {
             addTodoToLocalStorage(todoText);
+            appendTodoInHtml(todoText);
+            todoInput.value = '';
         }
 
     });
@@ -37,5 +51,11 @@ document.addEventListener("DOMContentLoaded", () => {
         
     });
 
-    loadTodos();
+    const todos = loadTodos();
+
+    todos.todoList.forEach(todo => {
+        const newTodoItem = document.createElement("li");
+        newTodoItem.textContent = todo;
+        todoList.appendChild(newTodoItem);
+    })
 });
